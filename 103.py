58b94a78b7f166b2e3dd36db1780919351469571
Daniel Wzoreck <wzoreck2000@gmail.com>
2021-05-20 23:20:46 -0300

    103.py Exercicios resolvidos

--- 103.py
+++ 103.py
@@ -6,73 +6,6 @@
     linguagem de programacao favorita. Nao use funcoes/metodos prontos.
 '''
 
-## STRINGS
-## https://docs.python.org/3/tutorial/introduction.html#strings
-
-msg = 'Minimal Techno Tripping'
-size = len(msg)
-print('Tamanho de msg:')
-print(size)
-
-## Converter para string
-s = str(42)
-print(s)
-
-s = 'I like you'
-print(s)
-
-## Examine as strings colocando prints
-s2 = s[0]  # retorna 'I'
-
-s2 = len(s)  # retorna 10
-
-# Como jah fizemos com as listas
-s2 = s[0:7]  # retorna 'I like '
-
-s2 = s[6:]  # retorna ' you'
-
-s2 = s[-1]  # retorna 'u'
-
-
-## concatenar strings
-s3 = 'The meaning of life is'
-s4 = '42'
-s5 = s3 + ' ' + s4       # retorna 'The meaning of life is 42'
-s5 = s3 + ' ' + str(42)  # same thing
-
-# split a string into a list of substrings separated by a delimiter
-
-s = 'Anything you want it to be'
-s.split(' ')        # retorna ['Anything', 'you', 'want', 'it', 'to', 'be']
-s.split()           # idem
-
-
-# Entrada via teclado (caracter de escape -> '\')
-print('What\'s your name?')
-nome = input()
-sobrenome = 'Abreu'
-print('Hi, ' + nome)
-print('Hi,', nome)
-
-# Formatacao com o metodo format
-msg = 'Hi, {} {}!'.format(sobrenome, nome)
-print(msg)
-msg = f'Hi, {sobrenome} {nome}!'
-print(msg)
-
-
-## Inverter a string
-string = 'Hello, my friend!'
-print(string)
-string2 = string[::-1]
-print(string2)
-
-## Substituir
-cheese_str = 'I like cheese'
-print(cheese_str)
-new_cheese_str = cheese_str.replace('like', 'love')
-print(new_cheese_str)
-
 ###
 # Exercicios
 ###
@@ -87,6 +20,39 @@ book1 = 'Homo Deus by Yuval Noah Harari, 2015'
 book2 = 'Antifragile by Nassim Nicholas Taleb, 2012'
 book3 = 'Fooled by Randomness by Nassim Nicholas Taleb, 2001'
 
+print()
+print('# 1)')
+print(book1[:book1.find('by')-1])
+print(book2[:book2.find('by')-1])
+print(book3[:book3.find('by')+13])
+
+print()
+print('# 2)')
+t1 = book1[:book1.find('by')-1]
+t2 = book2[:book2.find('by')-1]
+t3 = book3[:book3.find('by')+13]
+
+print(f'{t1}')
+print(t2)
+print(t3)
+
+print()
+print('# 3)')
+print(f'{len(t1)}')
+print(len(t2))
+print(len(t3))
+
+print()
+print('# 4)')
+autor = book1[book1.find('by')+3 : book1.find(',')]
+print(t1+' - '+autor+', '+book1[-4:])
+
+autor = book2[book2.find('by')+3 : book2.find(',')]
+print(t2+' - '+autor+', '+book2[-4:])
+
+autor = book3[book3.find('by')+17 : book3.find(',')]
+print(t3+' - '+autor+', '+book3[-4:])
+
 # 5) Verifique se uma palavra é uma palindrome perfeita.
 # Palindrome perfeito sao palavras que ao serem escritas em ordem reversa,
 # resultam na mesma palavra.
@@ -96,3 +62,27 @@ palindrome_one = 'ovo'
 palindrome_two = 'Natan'
 palindrome_three = 'luz azul'
 palindrome_four = 'caneta azul'
+
+print()
+print('# 5)')
+if(palindrome_one == palindrome_one[::-1]):
+	print(f'{palindrome_one} é palindrome perfeita')
+else:
+	print(f'{palindrome_one} não é palindrome perfeita')
+
+palindrome_two = palindrome_two.lower()
+if(palindrome_two == palindrome_two[::-1]):
+	print(f'{palindrome_two} é palindrome perfeita')
+else:
+	print(f'{palindrome_two} não é palindrome perfeita')
+
+palindrome_three = palindrome_three.lower()
+if(palindrome_three.replace(" ", "") == palindrome_three.replace(" ", "")[::-1]):
+	print(f'{palindrome_three} é uma palindrome perfeita')
+else:
+	print(f'{palindrome_three} não é uma palindrome perfeita')
+
+if(palindrome_four.replace(" ", "") == palindrome_four.replace(" ", "")[::-1]):
+	print(f'{palindrome_four} é uma palindrome perfeita')
+else:
+	print(f'{palindrome_four} não é uma palindrome perfeita')
