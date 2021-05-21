969ca24d2ee9659346e7654e2677b8b546ef464d
Daniel Wzoreck <wzoreck2000@gmail.com>
2021-05-21 01:00:20 -0300

    Exercicio 1 e 3 resolvidos

--- 106.py
+++ 106.py
@@ -112,5 +112,9 @@ print(courses)
 
 
 # 1) Implemente o metodo define_default_city de acordo com a docstring definida no inicio da funcao. Utilize a clausula else no loop implementado.
+
+
 # 2) Remova do arquivo capitais-BR.csv todas capitais dos estados do sudeste e teste se sua funcao estah robusta o suficiente. Ela deve executar sem erro mesmo que alguns dados estejam faltando.
+
+
 # 3) Faca uma funcao que le o arquivo lista-cpf.txt, retorne a quantidade de CPF unicos (sem repeticao) e os escreva em um arquivo lista-cpf-unicos.txt. Eh necessario descompactar o arquivo lista-cpf.txt.tar.gz primeiro. O algoritmo precisa ser eficiente, nesse caso especifico a melhor a melhor complexidade que pode ser acancada é linear. Algoritmos de complexidade quadratica, cubica, fatorial, etc. não sao considerados como eficientes pois a complexidade linear eh garantida. Como regra geral, se seu algoritmo demorar mais do que alguns segundos, ele, provavelmente, nao eh linear.
--- 107.py
+++ 107.py
@@ -1,94 +1,33 @@
 #!/usr/bin/python3
 
-import sys
-import timeit
-
-
-##
-# tuplas
-##
-
-## Tuplas x listas
-
-#  As tuplas sao estruturas semelhantes as listas; porem, as tuplas sao 'menores' e mais rapidas que as listas para se armazenar dados de maneira ordenada.
-# Ao contrario das listas, as tuplas sao imutaveis (immutable). Ou seja, uma vez que sao criadas, as tuplas sao estaticas e isso garante uma melhor performance nos casos em que podem ser aplicadas.
-# As listas demandam um maior custo, mas permitem adicionar, remover e alterar os seus dados. Ou seja, a lista é mutavel.
-
-# lista
-list = [2, 3, 5, 7, 11, 13, 17, 19]
-# tupla
-tuple = (2, 3, 5, 7, 11, 13, 17, 19)
-
-# mesmo tamanho
-print(len(list))
-print(len(tuple))
-
-# encerra o script sem saida de erro
-# comente ou mude de linha para estudar o resto do arquivo
-sys.exit(0)
-
-# iteracao identica
-for p in list:
-    print(p)
-
-for p in tuple:
-    print(p)
-
-# tamanho das tuplas eh bem menor. Pode ser significante ao se trabalhar com grande quantidade de dados
-print('Tamanho lista: ', sys.getsizeof(list))
-print('Tamanho tupla: ', sys.getsizeof(tuple))
-
-# tamanho de um range (lembram da ultima aula?)
-print('Tamanho range: ', sys.getsizeof(range(9)))
-print('Tamanho range: ', sys.getsizeof(range(999999999999999999)))
-
-# por ser imutavel, as tuplas sao criadas mais rapidamente
-# tempo para cria um milhao de listas e tuplas (aumente essa quantidade, progressivamente, para perceber o efeito)
-list_time = timeit.timeit(stmt="[1,2,3,4,5]", number=1_000_000)
-tuple_time = timeit.timeit(stmt="(1,2,3,4,5)", number=1_000_000)
-
-
-print('Tempo lista:', list_time)
-print('Tempo tupla:', tuple_time)
-percent = tuple_time / list_time * 100
-print(f'{percent}% mais rapido')
-
-
-# as tuplas nao precisam dos parenteses para serem criadas
-t1 = (1, 2)
-t2 = 1, 2
-
-print(type(t1))
-print(type(t2))
-print(t1 == t2)
-
-# Unpack de tupla. Formato (idade, cidade, vacinado)
-survey = (22, 'Canoinhas', False)
-age, city, vaccined = survey
-
-print(f'idade: {age}; cidade: {city}; vacinado: {vaccined}')
-
-# Acesso por indice
-print(survey[2])
-
-# Detalhes
-empty_tuple = ()
-tuple1 = ('teste1')
-tuple2 = ('teste1', 'teste2')
-
-print(type(empty_tuple))
-print(type(tuple1))
-print(type(tuple2))
-
-# Forcar tuple1 a ser uma tupla
-
-tuple1 = ('teste1', )
-print(type(tuple1))
-
 ###
 # Exercicio
 ###
 
 # 1) Imprima os metodos disponiveis para uma lista e para uma tupla
+print('# 1)')
+print()
+print(dir(list))
+print()
+print(dir(tuple))
+
 # 2) Faca um metodo para retornar apenas as diferencas entre as duas de metodos
+
 # 3) Adicione as coordenadas (latitude, longitude) para os dicts professor1, professor2 e professor3. Copie os dicts do arquivo 106.py. As coordenadas precisam ser inseparaveis e imutaveis.
+print()
+print('# 3)')
+
+print()
+professor1 = {'id': 42, 'name': 'Alexandre Abreu', 'age': 30, 'state_origin': 'Minas Gerais', 'courses': ['Inteligência Artificial', 'Mineração de Dados', 'Programação para Internet I', 'Programação para Internet II'], 'latitude':('234.4534.5464'), 'longitude':('5436.57568.46')}
+
+professor2 = {'id': 37, 'name': 'Denilson Barbosa', 'age': 40, 'state_origin': 'Paraná', 'courses': ['Inteligência Artificial', 'Banco de Dados I', 'Banco de Dados II', 'Programação para Internet I'], 'laitude':('123421.43534.4'), 'longitude':('32423.235346.54')}
+
+professor3 = dict(id=28, name='Jorge Armino', idade=37)
+professor3['state_origin'] = 'Rio Grande do Sul'
+professor3['courses'] = ['Filosofia', 'Informática e Sociedade']
+professor3['latitude'] = ('234.4534.5464')
+professor3['longitude'] = ('5436.57568.46')
+
+print(professor1)
+print(professor2)
+print(professor3)
