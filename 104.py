ed4559f3939160b39d897adf579edd3345ad2747
Daniel Wzoreck <wzoreck2000@gmail.com>
2021-05-21 00:21:29 -0300

    104.py Exercicios feitos

--- 104.py
+++ 104.py
@@ -7,78 +7,46 @@
     OBS.: Use sua linguagem de programacao favorita. Nao use funcoes/metodos prontos.
 '''
 
-## Funcoes
-
-# Define a funcao
-def sum(x=3, y=5):
-    print("x: " + str(x))
-    print("y: " + str(y))
-    return x + y
-
-# Chamada simples de funcao
-x = 1
-y = 2
-z = sum(x, y)
-print(z)
-
-# Chamada de funcao com parametro sem ordem
-z = sum(y=10, x=13)
-print(z)
-
-# Utiliza valor padrao dos parametros
-z = sum()
-print(z)
-
-# PACKING E UNPACKING
-# Atribuicao para mais de uma variável
-a, b = x, y
-print(f'a: {a}, b:{b}')
-
-list = [1, 2, 3]
-a, b, c = list
-print(f'a: {a}, b:{b}, c:{c}')
-
-# Ignorando um valor
-a, b, _ = list
-print(f'a: {a}, b:{b},')
-
-# Multiplos valores (*)
-a, *resto = list
-print(f'a: {a}, resto: {resto}')
-
-# troca de valores
-a, b = b, a
-print(f'a: {a}, b:{b}')
-
-
-## CONDICIONAIS
-
-temperature = 4
-if temperature <= 0:
-    print('Solido')
-elif temperature > 0 and temperature < 100:
-    print('Liquido')
-else:
-    print('Gasoso')
-
-# Notacao matematica
-if 0 < temperature < 100:
-    print('Liquido')
-
-
-## Operador ternario
-# var = resultado_condicao_verdadeitra if condicao else resultado_condicao_false
-valor = 4
-positivo = True if valor > 0 else False
-print(positivo)
-
-
 ###
 # Exercicios
 ###
 
 # 1) Crie uma funcao que receba duas listas e verifique se elas são iguais. Cada elemento e sua ordem devem ser o mesmo. Retorne True ou False.
-#
+print('1) Crie uma funcao que receba duas listas e verifique se elas são iguais. Cada elemento e sua ordem devem ser o mesmo. Retorne True ou False.')
+
+def comparaListas(a=[], b=[]):
+	if(a[:] == b[:]):
+		return True
+	else:
+		return False
+	
+lista1 = [1, 2, 3]
+lista2 = [1, 2, 3]
+print(f'Retorno função comparaListas: {comparaListas(lista1, lista2)}')
+
+l1 = ['Daniel', 'Wzoreck']
+l2 = ['Daniel', 'Wzoreck']
+print(f'Retorno função comparaListas: {comparaListas(l1, l2)}')
+
+l3 = ['Daniel', 'Wzorek']
+l4 = ['Daniel', 'Wzoreck']
+print(f'Retorno função comparaListas: {comparaListas(l3, l4)}')
+
+print(f'Retorno função comparaListas: {comparaListas(l1, lista1)}')
+
 # 2) Crie uma funcao que verifica se duas strings são palindromes perfeitas. Faca as 'limpeza'/sanitizacao necessarias.  Retorne True ou False.
-#
+print('2) Crie uma funcao que verifica se duas strings são palindromes perfeitas. Faca as limpeza/sanitizacao necessarias.  Retorne True ou False.')
+
+def palindromePerfeita(a = str()):
+	if(a.lower().replace(" ", "") == a.lower().replace(" ", "")[::-1]):
+		return True
+	else:
+		return False
+
+a = 'LUz AzuL'
+print(f'Retorno função palindromePerfeita: {palindromePerfeita(a)}')
+
+a = 'na na'
+print(f'Retorno função palindromePerfeita: {palindromePerfeita(a)}')
+
 # OBS.: Utilize apenas o que foi estudado ate agora
